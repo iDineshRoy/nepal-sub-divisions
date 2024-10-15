1bf44b24ae43a19b02eeacb8a080e62db886662f
Dinesh Kumar Roy <dinesh.roy@hotmail.com>
2024-10-15 23:18:49 +1100

    fix version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="nepal-sub-divisions",
-    version="0.0.5",  # Required
+    version="0.0.4",  # Required
     description="Nepali  municipalities is a python package to get data about Nepali municipalities based on districts",
     url="https://github.com/iDineshRoy/nepal-sub-divisions",
     long_description=long_description,
