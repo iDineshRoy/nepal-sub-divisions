56fa0b9f064a80990d1a730ca6095166fd3e554f
Dinesh Kumar Roy <dinesh.roy@hotmail.com>
2025-04-02 00:04:15 +1100

    new updates

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
     author_email="dinesh.roy@hotmail.com",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    license_file="LICENSE",
     package_data={
         "nepal_municipalities": ["data/*.json"],
     },
